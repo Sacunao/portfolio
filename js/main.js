8b09e6a84db82ddaa6ecda8a1770b219475c49dd
Anderson Acuña Obregon <Anderson Acua Obregon>
2016-11-26 18:13:10 -0500

    modificaci¢n navbar, filter y meta twitter

--- js/main.js
+++ js/main.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 	new WOW().init();
 
-	 $(".filter-button").click(function(){
+    var scroll_start = 0;
+    var startchange = $('#start');
+    var offset = startchange.offset();
+    if (startchange.length){
+       $(document).scroll(function() { 
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+              $(".navbar-default").css('background-color', '#231F20');
+           } else {
+              $('.navbar-default').css('background-color', 'transparent');
+           }
+       });
+    }
+
+	$(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
         
         if(value == "all")
         {
-            //$('.filter').removeClass('hidden');
             $('.filter').show('1000');
         }
         else
         {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
             $(".filter").not('.'+value).hide('3000');
             $('.filter').filter('.'+value).show('3000');
             
         }
     });
+
+   
+
 });
 
 

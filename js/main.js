ce3b3d5d14a363256fae5e557d5f66cc985e4ab5
sacunao <sheila.aobregon@gmail.com>
2016-12-01 15:08:10 -0500

    modificación navbar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 $(document).ready(function(){
 	new WOW().init();
 
-    var scroll_start = 0;
-    var startchange = $('#start');
-    var offset = startchange.offset();
-    if (startchange.length){
-       $(document).scroll(function() { 
-          scroll_start = $(this).scrollTop();
-          if(scroll_start > offset.top) {
-              $(".navbar-default").css('background-color', '#231F20');
-           } else {
-              $('.navbar-default').css('background-color', 'transparent');
-           }
-       });
-    }
+  var scroll_start = 0;
+  var startchange = $('#start');
+  var offset = startchange.offset();
+
+  if($("body").offset().top > offset){
+    opaqueNav();
+  }else{
+    clearNav();
+  }
+
+  $(document).scroll(function() { 
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            opaqueNav();
+         } else {
+            clearNav();
+         }
+  });
+
+
+  function opaqueNav() {
+    $(".navbar-default").css('background-color', '#231F20');
+  }
+
+  function clearNav(){
+    $('.navbar-default').css('background-color', 'transparent');
+  }
+  
 
 	$(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
